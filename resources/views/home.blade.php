78412308a25176754f693feb0c83cb63c8f62df4
Jobanpreet Singh <210142187@aston.ac.uk>
2024-10-06 17:15:04 +0100

    added home html and css

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!-- http://localhost/amsproject/resources/views/home.blade.php -->
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>AMS</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="../../resources/css/home.css">
+
+    </head>
+    <body class="home-body">
+        <p>Hiii</p>
+        
+    </body>
+</html>
